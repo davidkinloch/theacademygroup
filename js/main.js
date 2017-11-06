5f88688e0d3773e8f3341c2346136b6f65ee56c1
David Kinloch <dkinloch@gmail.com>
2017-11-06 21:14:04 +0100

    full screne mobile menu

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ $(function() {
     	e.preventDefault();
     	$(this).toggleClass('active');
     	$('.nav').toggleClass('active');
+    	$('body').toggleClass('fixed');
   	});
   	$('.nav__close').click( function() {
     	$('.nav__toggle').removeClass('active');
     	$('.nav').removeClass('active');
+    	$('body').removeClass('fixed');
     });	
 });
