a233b5fd6bfbd37b538296b03e837d2c4ee73b5f
David Kinloch <dkinloch@gmail.com>
2017-11-11 12:49:24 +0000

    Added team template

--- js/main.js
+++ js/main.js
@@ -10,5 +10,10 @@ $(function() {
     	$('.nav__toggle').removeClass('active');
     	$('.nav').removeClass('active');
     	$('body').removeClass('fixed');
-    });	
+    });
+    $('.team__more').click( function(e) {
+        e.preventDefault();
+        $('.team__list--more').addClass('active');
+        $(this).addClass('destroy');
+     });
 });
